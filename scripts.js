717562b11d5b91e5833c8a4d3c9ee9ff0da05828
Kate <katherine.a.salesin@dartmouth.edu>
2025-10-17 17:27:40 -0400

    rotation parallax

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 // Get the element you want to rotate
-const elements = document.getElementsByClassName("kaleidoscope-inner");
+const elements1 = document.getElementsByClassName("kaleidoscope-inner");
+const elements2 = document.getElementsByClassName("kaleidoscope-inner-flowers");
 
 // Add a scroll event listener to the window
 window.addEventListener('scroll', () => {
@@ -9,22 +10,33 @@ window.addEventListener('scroll', () => {
     // Calculate the rotation angle based on scroll position
     // You can adjust the divisor to control the rotation speed
     const rotationAngleStart = 30;
-    const rotationAngle = rotationAngleStart + (scrollPosition / 50); // Example: 1 degree per 5px scrolled
+    const rotationAngle1 = rotationAngleStart + (scrollPosition / 50); // Example: 1 degree per 5px scrolled
+    const rotationAngle2 = rotationAngleStart + (scrollPosition / 25); // Example: 1 degree per 5px scrolled
 
-    for (let i = 0; i < elements.length; i++) {
-        const element = elements[i];
+    for (let i = 0; i < elements1.length; i++) {
+        const element1 = elements1[i];
+        const element2 = elements2[i];
 
         if (i == 0) {
             // Apply the rotation to the element's transform style
-            element.style.transform = `rotate(${rotationAngle}deg)`;
+            element1.style.transform = `rotate(${rotationAngle1}deg)`;
         } else if (i == 1) {
-            element.style.transform = `scaleX(-1) rotate(${rotationAngle}deg)`;
+            element1.style.transform = `scaleX(-1) rotate(${rotationAngle1}deg)`;
         } else if (i == 2) {
-            element.style.transform = `scaleY(-1) rotate(${rotationAngle}deg)`;
+            element1.style.transform = `scaleY(-1) rotate(${rotationAngle1}deg)`;
         } else {
-            element.style.transform = `scaleX(-1) scaleY(-1) rotate(${rotationAngle}deg)`;
+            element1.style.transform = `scaleX(-1) scaleY(-1) rotate(${rotationAngle1}deg)`;
+        }
+
+        if (i == 0) {
+            // Apply the rotation to the element's transform style
+            element2.style.transform = `rotate(${rotationAngle2}deg)`;
+        } else if (i == 1) {
+            element2.style.transform = `scaleX(-1) rotate(${rotationAngle2}deg)`;
+        } else if (i == 2) {
+            element2.style.transform = `scaleY(-1) rotate(${rotationAngle2}deg)`;
+        } else {
+            element2.style.transform = `scaleX(-1) scaleY(-1) rotate(${rotationAngle2}deg)`;
         }
     }
-
-    console.log(rotationAngle);
 });
